8b64528d4224a5b4869e537f835ad38907cf2f54
Hugues Nouvel <70510644+HunoPPC-NG@users.noreply.github.com>
2022-04-21 12:05:50 +0200

    Add files via upload

---
+++ Samples/GLUes-Demos/simple-texture 2d-GLUes/simple-texture2d.c
@@ -0,0 +1,247 @@
+//
+// Book:      OpenGL(R) ES 2.0 Programming Guide
+// Authors:   Aaftab Munshi, Dan Ginsburg, Dave Shreiner
+// ISBN-10:   0321502795
+// ISBN-13:   9780321502797
+// Publisher: Addison-Wesley Professional
+// URLs:      http://safari.informit.com/9780321563835
+//            http://www.opengles-book.com
+//
+
+// Simple_Texture2D.c
+//
+//    This is a simple example that draws a quad with a 2D
+//    texture image. The purpose of this example is to demonstrate
+//    the basics of 2D texturing
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "EGL/egl.h"
+#include "EGL/glues.h"
+
+
+typedef struct {
+    // Handle to a program object
+    GLuint programObject;
+
+    // Attribute locations
+    GLint  positionLoc;
+    GLint  texCoordLoc;
+
+    // Sampler location
+    GLint samplerLoc;
+
+    // Texture handle
+    GLuint textureId;
+
+    GLuint vertexObject, indexObject;
+} UserData;
+
+static UserData _UserData;
+
+//
+// Create a simple 2x2 texture image with four different colors
+//
+GLuint CreateSimpleTexture2D()
+{
+    // Texture object handle
+    GLuint textureId;
+
+    // 2x2 Image, 4 bytes per pixel (R, G, B, A)
+    GLubyte pixels[4 * 4] =
+        {
+            255,   0,   0, 255,// Red
+            0, 255,   0, 255,// Green
+            0,   0, 255, 255,// Blue
+            255, 255,  0 , 255// Yellow
+        };
+
+    // Use tightly packed data
+    glPixelStorei ( GL_UNPACK_ALIGNMENT, 1 );
+
+    // Generate a texture object
+    glGenTextures ( 1, &textureId );
+
+    // Bind the texture object
+    glBindTexture ( GL_TEXTURE_2D, textureId );
+
+    // Load the texture
+    glTexImage2D ( GL_TEXTURE_2D, 0, GL_RGBA, 2, 2, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels );
+
+    // Set the filtering mode
+    glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+    glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+
+    return textureId;
+
+}
+
+
+//
+// Initialize the shader and program object
+//
+int Init (ESContext *esContext)
+{
+    UserData *userData = &_UserData;
+    char vShaderStr[] =
+        "attribute vec4 a_position;   \n"
+        "attribute vec2 a_texCoord;   \n"
+        "varying vec2 v_texCoord;     \n"
+        "void main()                  \n"
+        "{                            \n"
+        "   gl_Position = a_position; \n"
+        "   v_texCoord = a_texCoord;  \n"
+        "}                            \n";
+
+    char fShaderStr[] =
+        "precision mediump float;                            \n"
+        "varying vec2 v_texCoord;                            \n"
+        "uniform sampler2D s_texture;                        \n"
+        "void main()                                         \n"
+        "{                                                   \n"
+        "  gl_FragColor = texture( s_texture, v_texCoord );\n"
+        "}                                                   \n";
+
+    memset(&_UserData, 0, sizeof(_UserData));
+
+    // Load the shaders and get a linked program object
+    userData->programObject = esLoadProgram ( vShaderStr, fShaderStr );
+
+    // Get the attribute locations
+    userData->positionLoc = glGetAttribLocation ( userData->programObject, "a_position" );
+    userData->texCoordLoc = glGetAttribLocation ( userData->programObject, "a_texCoord" );
+
+    // Get the sampler location
+    userData->samplerLoc = glGetUniformLocation ( userData->programObject, "s_texture" );
+
+    /* Important Link the program object. */
+    glLinkProgram ( userData->programObject);
+
+    // Load the texture
+    userData->textureId = CreateSimpleTexture2D ();
+
+    // Setup the vertex data
+    {
+        GLfloat vVertices[] = {-0.5f,  0.5f, 0.0f, 1.0f,  // Position 0
+                            0.0f,  0.0f,        // TexCoord 0 
+                           -0.5f, -0.5f, 0.0f, 1.0f,  // Position 1
+                            0.0f,  1.0f,        // TexCoord 1
+                            0.5f, -0.5f, 0.0f, 1.0f,  // Position 2
+                            1.0f,  1.0f,        // TexCoord 2
+                            0.5f,  0.5f, 0.0f, 1.0f,  // Position 3
+                            1.0f,  0.0f         // TexCoord 3
+        };
+        GLushort indices[] = { 0, 1, 2, 0, 2, 3 };
+
+        glGenBuffers(1, &userData->vertexObject);
+        glBindBuffer(GL_ARRAY_BUFFER, userData->vertexObject );
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vVertices), vVertices, GL_STATIC_DRAW );
+
+        glGenBuffers(1, &userData->indexObject);
+        glBindBuffer ( GL_ELEMENT_ARRAY_BUFFER, userData->indexObject );
+        glBufferData ( GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW );
+    }
+
+    glClearColor ( 0.0f, 0.0f, 0.0f, 1.0f );
+    return GL_TRUE;
+}
+
+void Reshape(ESContext *esContext, int width, int height) {
+    /* glutLogMessage("I/Reshape: %d, %d",width,height); */
+    // Set the viewport
+    glViewport ( 0, 0, width, height );
+}
+
+//
+// Draw a triangle using the shader pair created in Init()
+//
+void Draw(ESContext *esContext)
+{
+    UserData *userData = &_UserData;
+
+    // Clear the color buffer
+    glClear ( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+    // Use the program object
+    glUseProgram ( userData->programObject );
+
+    // Load the vertex position
+    glBindBuffer (GL_ARRAY_BUFFER, userData->vertexObject );
+    glVertexAttribPointer ( userData->positionLoc, 4, GL_FLOAT,
+                            GL_FALSE, 6 * 4, 0 );
+    // Load the texture coordinate
+    glVertexAttribPointer ( userData->texCoordLoc, 2, GL_FLOAT,
+                            GL_FALSE, 6 * 4,
+                            (void*)(4 * 4) );
+
+    glEnableVertexAttribArray ( userData->positionLoc );
+    glEnableVertexAttribArray ( userData->texCoordLoc );
+
+    // Bind the texture
+    glActiveTexture ( GL_TEXTURE0 );
+    glBindTexture ( GL_TEXTURE_2D, userData->textureId );
+
+    // Set the sampler texture unit to 0
+    glUniform1i ( userData->samplerLoc, 0 );
+
+    glBindBuffer ( GL_ELEMENT_ARRAY_BUFFER, userData->indexObject );
+    glDrawElements ( GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0 );
+
+    glutSwapBuffers();
+}
+
+/**
+ * Handles special glut events.
+ *
+ * @param special the event to handle.
+ */
+static void
+keys_special(ESContext *esContext, int special, int crap, int morecrap)
+{
+   switch (special) {
+      case GLUT_KEY_1:
+         esActivateFPS(1);
+         break;
+      case GLUT_KEY_0:
+         esActivateFPS(0);
+         break;
+      case GLUT_KEY_ESCAPE:
+    //New function for Unload context and shutdown a lib EGL for Opengl-ES AOS4
+       esShutdown(esContext);
+	   break;
+   }
+   
+}
+
+
+int main ( int argc, char *argv[] )
+{
+    ESContext esContext;
+
+
+   glutInit(&esContext, &argc,argv);
+
+
+    glutInitWindowSize(512,512);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    if (!glutCreateWindow(&esContext, "Simple_Texture2D GLUes with lib EGL for Opengl-ES AmigaOS4")) {
+        glutLogMessage("E/ Can't create GLUes window !");
+        return -1;
+    }
+    
+    glutSpecialFunc(&esContext, keys_special);
+    glutReshapeFunc(&esContext,Reshape);
+    glutDisplayFunc(&esContext,Draw);
+
+    if (!Init(&esContext)) {
+        glutLogMessage("E/ Can't initialize test GLUes!");
+        return -1;
+    }
+
+    glutMainLoop(&esContext);
+
+
+
+    return 0;
+}
