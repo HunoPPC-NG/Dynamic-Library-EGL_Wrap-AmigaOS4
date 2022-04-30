acec046c6726598bdab3e866948992561b242273
Hugues Nouvel <70510644+HunoPPC-NG@users.noreply.github.com>
2022-04-30 13:29:11 +0200

    Delete libegl_wrap directory

--- libegl_wrap/SDK/include/EGL/eglext.h
+++
@@ -1,8 +0,0 @@
-/* EGL Wrapper lib for Opengl-ES-NOVA Written by Hugues 'HunoPPC' Nouvel 2022 Amiga French Team Power!!*/
-#ifndef __eglext_h_
-#define __eglext_h_ 
-
-
-#include <EGL/egl_wrap.h>
-
-#endif /* __eglext_h_ */ 
--- libegl_wrap/SDK/include/EGL/eglplatform.h
+++
@@ -1,9 +0,0 @@
-/* EGL Wrapper lib for Opengl-ES-NOVA Written by Hugues 'HunoPPC' Nouvel 2022 Amiga French Team Power!!*/
-#ifndef __eglplatform_h_
-#define __eglplatform_h_
-
- 
-#include <EGL/egl_wrap.h>
-
-
-#endif /* __eglplatform_h */ 
